2f57ec2b294e50889838b81e93b80cbb4ab861eb
Carl <c@cem.me>
2012-12-10 07:55:37 -0600

    Update certpins.go
    add drag and drop functionality

--- certpins.go
+++ certpins.go
@@ -34,55 +34,47 @@ func init() {
 
 //blob to write to top of every page
 const head =`
+<!DOCTYPE html>
 <html>
-  <head><link href='http://fonts.googleapis.com/css?family=Anaheim' rel='stylesheet' type='text/css'>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Pin Creator</title>
+
   <style media="screen" type="text/css">
-html,
-	body {
-		margin:0;
-		padding:0;
-		height:100%;
+  html,
+	
+	#header {
+		padding:10px;
+	}
+	#body {
+		padding:10px;
+		padding-bottom:2em;	/* Height of the footer */
+	}
+	#footer {
+		position:absolute;
+		bottom:0;
+		height:2em;			/* Height of the footer */
 	}
 	#container {
 		min-height:100%;
 		position:relative;
 	}
-	#header {
-		padding:10px;
-
-    max-width: 50em;
-    margin-left: auto;
-    margin-right: auto;
-	}
-	#body {
-		padding:10px;
-		padding-bottom:2em;	/* Height of the footer */
-    max-width: 50em;
-    margin-left: auto;
-    margin-right: auto
-	}
-	#footer {
-		position:absolute;
-		bottom:0;
-		width:100%;
-		height:2em;			/* Height of the footer */
-
-	}
   
   </style>
   </head>
-  <body><div id="container">
+  <body>
+  <div id="container">
   <div id="header">
     <h1 style="font-family: 'Anaheim', sans-serif;">Certificate Pinning - Pin Creator</h1>
     <h5 style="padding-top:0em;margin-top:0em"><a href="/">home</a>&emsp;<a href="/about">about this tool</a></h5>
-    <br />
   </div>
   <div id="body">
 `
 //blob to write to bottom of every page
 const foot = `
     </div>
-    <div id="footer"><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/80x15.png" /></a> 2012 <a href="https://plus.google.com/115606090533118002214?rel=author">Carl Mehner</a></div>
+    <br />
+    <div id="footer">Copyright &copy; 2012 <a href="https://plus.google.com/115606090533118002214?rel=author">Carl Mehner</a></div>
     </div>
   </body>
 </html>
@@ -90,9 +82,36 @@ const foot = `
 
 //form to paste in your certificate
 const certForm=`
-    <p>Paste in a certificate below, PEM encoded with base64, including the BEGIN and END headers</p>
+<p>Drag and Drop a certificate file or paste in a certificate below.</p><p>Make sure the certificate is PEM, encoded with base64, and that they include the BEGIN and END headers</p>
     <form action="/pin" method="post">
-      <div><textarea name="cert" rows="20" cols="70" spellcheck="false" style="monospace"></textarea></div>
+      <div>
+        <textarea name="cert" id="cert_drop" rows="20" cols="70" spellcheck="false" style="monospace;"></textarea>
+<script>
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files; // FileList object.
+    var reader = new FileReader();  
+    reader.onload = function(event) {            
+         document.getElementById('cert_drop').value = event.target.result;
+    }        
+    reader.readAsText(files[0],"UTF-8");
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  // Setup the dnd listeners.
+  var dropZone = document.getElementById('cert_drop');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+</script>
+
+      </div>
       <div><input type="submit" value="Create Cert Pin"></div>
     </form>
 `
@@ -106,6 +125,11 @@ Resources to find out more about pinning are included below:</p>
 <li /><a href="http://www.imperialviolet.org/2011/05/04/pinning.html">http://www.imperialviolet.org/2011/05/04/pinning.html</a>
 <li /><a href="http://www.thoughtcrime.org/blog/authenticity-is-broken-in-ssl-but-your-app-ha/">http://www.thoughtcrime.org/blog/authenticity-is-broken-in-ssl-but-your-app-ha/</a>
 <p>This tool builds upon the go implementation of cert pinning in the key-pinning draft.</p>
+
+<br />
+<h2>Example Output</h2>
+<h3>Certificate Pin created for www.google.com</h3><p>Go to <tt>chrome://net-internals/#hsts</tt> to manually add it to your chrome install</p><p><b>sha1/PH28jUH5JV3EJIKi5wVf64OTZK4=</b></p><p>and/or add it to your <a href="http://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security#Implementation">website's headers</a> using a method similar to HSTS (HTTP Strict Transport Security).<br />You <a href="http://tools.ietf.org/html/draft-ietf-websec-key-pinning#section-3.1">MUST</a> have a backup pin if you are using this method, otherwise, the browser will ignore the pin attempt.</p><p><b>Public-Key-Pins: max-age=31536000;<br />&emsp;pin-sha1="PH28jUH5JV3EJIKi5wVf64OTZK4=";<br />&emsp;pin-sha256="dhPKqMNshz05+vFbUc5C2HmcXReO04Fi+LtdzlydVD0=";</b></p>
+<hr />
 <p>
   <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
     <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
