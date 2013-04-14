80f2d7f7c43381c8e2261122343a087990887a11
Carl <c+git@cem.me>
2013-04-14 14:39:13 -0500

    support for AndroidPinning
    added support for AndroidPinning and edited some wording and css.

--- certpins.go
+++ certpins.go
@@ -1,4 +1,4 @@
-//Copyright (C) 2012 Carl Mehner
+//Copyright (C) 2013 Carl Mehner
 
 //This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -59,7 +59,9 @@ const head =`
 		min-height:100%;
 		position:relative;
 	}
-  
+	p.output {
+		padding-left:50px;
+	}
   </style>
   </head>
   <body>
@@ -74,7 +76,7 @@ const head =`
 const foot = `
     </div>
     <br />
-    <div id="footer">Copyright &copy; 2012 <a href="https://plus.google.com/115606090533118002214?rel=author">Carl Mehner</a></div>
+    <div id="footer">Copyright &copy; 2013 <a href="https://plus.google.com/115606090533118002214?rel=author">Carl Mehner</a></div>
     </div>
   </body>
 </html>
@@ -121,27 +123,27 @@ const aboutInfo=`
 <p>Certificate pinning is a way to tell clients what cert or CA
 they should be seeing when they connect to your website.<br />
 Resources to find out more about pinning are included below:</p>
-<li /><a href="http://tools.ietf.org/html/draft-ietf-websec-key-pinning">http://tools.ietf.org/html/draft-ietf-websec-key-pinning</a>
+<li /><a href="https://tools.ietf.org/html/draft-ietf-websec-key-pinning">https://tools.ietf.org/html/draft-ietf-websec-key-pinning</a>
 <li /><a href="http://www.imperialviolet.org/2011/05/04/pinning.html">http://www.imperialviolet.org/2011/05/04/pinning.html</a>
 <li /><a href="http://www.thoughtcrime.org/blog/authenticity-is-broken-in-ssl-but-your-app-ha/">http://www.thoughtcrime.org/blog/authenticity-is-broken-in-ssl-but-your-app-ha/</a>
 <p>This tool builds upon the go implementation of cert pinning in the key-pinning draft.</p>
 
 <br />
 <h2>Example Output</h2>
-<h3>Certificate Pin created for www.google.com</h3><p>Go to <tt>chrome://net-internals/#hsts</tt> to manually add it to your chrome install</p><p><b>sha1/PH28jUH5JV3EJIKi5wVf64OTZK4=</b></p><p>and/or add it to your <a href="http://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security#Implementation">website's headers</a> using a method similar to HSTS (HTTP Strict Transport Security).<br />You <a href="http://tools.ietf.org/html/draft-ietf-websec-key-pinning#section-3.1">MUST</a> have a backup pin if you are using this method, otherwise, the browser will ignore the pin attempt.</p><p><b>Public-Key-Pins: max-age=31536000;<br />&emsp;pin-sha1="PH28jUH5JV3EJIKi5wVf64OTZK4=";<br />&emsp;pin-sha256="dhPKqMNshz05+vFbUc5C2HmcXReO04Fi+LtdzlydVD0=";</b></p>
-<hr />
+<h3>Certificate Pin created for www.google.com</h3><h4>To install in Chrome:</h4><p class="output">Go to <tt>chrome://net-internals/#hsts</tt> to manually add it to your chrome install</p><p class="output"><b>sha1/PH28jUH5JV3EJIKi5wVf64OTZK4=</b></p><h4>To use on your website:</h4><p class="output">Add to your <a href="https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security#Implementation">website's headers</a> using a method similar to HSTS (HTTP Strict Transport Security).<br />You <a href="https://tools.ietf.org/html/draft-ietf-websec-key-pinning#section-3.1">MUST</a> have a backup pin if you are using this method, otherwise, the browser will ignore the pin attempt.<br />You should get either a backup certificate, or the CA's cert that signs your server certs and run it again below<br />and add the line that starts with pin-sha256 into the headers you are sending to your clients.</p><p class="output"><b>Public-Key-Pins: max-age=31536000;<br />&emsp;pin-sha256="dhPKqMNshz05+vFbUc5C2HmcXReO04Fi+LtdzlydVD0=";</b></p><h4>To use in your Android app using Moxie's <a href="https://github.com/moxie0/AndroidPinning">AndroidPinning</a> library:</h4><p class="output">...<br />PinningTrustManager(new String[] {"<b>63f1353dd35fe084d20627b7861320d02aa18f14</b>"});<br />...</p><hr />
 <p>
-  <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
-    <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
+  <a rel="license" href="https://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
+    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
   </a>
   <br />
-    <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">CertPins</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://plus.google.com/115606090533118002214" rel="author" property="cc:attributionName" rel="cc:attributionURL">Carl Mehner</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.<br />
+    <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">CertPins</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://plus.google.com/115606090533118002214" rel="author" property="cc:attributionName" rel="cc:attributionURL">Carl Mehner</a> is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.<br />
     Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/cem-/certpins" rel="dct:source">https://github.com/cem-/certpins</a>.
   </p>
 `
 
 //about page
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Strict-Transport-Security", "max-age=31536000")
   fmt.Fprint(w, head)
   fmt.Fprint(w, aboutInfo)
   fmt.Fprint(w, foot)
@@ -149,6 +151,7 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 
 //main page
 func handler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Strict-Transport-Security", "max-age=31536000")
   fmt.Fprint(w, head)
   fmt.Fprint(w, certForm)
   fmt.Fprint(w, foot)
@@ -158,6 +161,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func pinHandler(w http.ResponseWriter, r *http.Request) {
   //must parse before reading any data
   err := r.ParseForm()
+  w.Header().Set("Strict-Transport-Security", "max-age=31536000")
   fmt.Fprint(w, head)
   if err != nil {
       fmt.Fprint(w, "<p>Error Parsing Form Data: ")
@@ -189,12 +193,13 @@ func pinHandler(w http.ResponseWriter, r *http.Request) {
       digest := h.Sum(nil)
       digest2 := h2.Sum(nil)
       // print out in a form for chrome to use
-      fmt.Fprint(w, "<h3>Certificate Pin created for " + cert.Subject.CommonName + "</h3>")
-      fmt.Fprint(w, "<p>Go to <tt>chrome://net-internals/#hsts</tt> to manually add it to your chrome install</p>")
-      fmt.Fprintf(w, "<p><b>sha1/%s</b></p>", base64.StdEncoding.EncodeToString(digest))
+      fmt.Fprint(w, "<h3>Certificate Pin created for " + cert.Subject.CommonName + "</h3><h4>To install in Chrome:</h4>")
+      fmt.Fprint(w, "<p  class=\"output\">Go to <tt>chrome://net-internals/#hsts</tt> to manually add it to your chrome install</p>")
+      fmt.Fprintf(w, "<p class=\"output\"><b>sha1/%s</b></p>", base64.StdEncoding.EncodeToString(digest))
       // print out in a form conforming to the key pinning draft
-      fmt.Fprint(w, "<p>and/or add it to your <a href=\"http://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security#Implementation\">website's headers</a> using a method similar to HSTS (HTTP Strict Transport Security).<br />You <a href=\"http://tools.ietf.org/html/draft-ietf-websec-key-pinning#section-3.1\">MUST</a> have a backup pin if you are using this method, otherwise, the browser will ignore the pin attempt.</p>")
-      fmt.Fprintf(w, "<p><b>Public-Key-Pins: max-age=31536000;<br />&emsp;pin-sha1=\"%s\";<br />&emsp;pin-sha256=\"%s\";</b></p>", base64.StdEncoding.EncodeToString(digest), base64.StdEncoding.EncodeToString(digest2))
+      fmt.Fprint(w, "<h4>To use on your website:</h4><p class=\"output\">Add to your <a href=\"https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security#Implementation\">website's headers</a> using a method similar to HSTS (HTTP Strict Transport Security).<br />You <a href=\"https://tools.ietf.org/html/draft-ietf-websec-key-pinning#section-3.1\">MUST</a> have a backup pin if you are using this method, otherwise, the browser will ignore the pin attempt.<br />You should get either a backup certificate, or the CA's cert that signs your server certs and run it again below<br />and add the line that starts with pin-sha256 into the headers you are sending to your clients.</p>")
+      fmt.Fprintf(w, "<p class=\"output\"><b>Public-Key-Pins: max-age=31536000;<br />&emsp;pin-sha256=\"%s\";</b></p>", base64.StdEncoding.EncodeToString(digest2))
+      fmt.Fprintf(w, "<h4>To use in your Android app using Moxie's <a href=\"https://github.com/moxie0/AndroidPinning\">AndroidPinning</a> library:</h4><p class=\"output\">...<br />PinningTrustManager(new String[] {\"<b>%x</b>\"});<br />...</p>", digest)
     }
   }
 
